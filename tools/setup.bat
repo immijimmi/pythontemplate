1e9982e9633b38eb59e4eb0563b4f4c362e7c045
immijimmi <immijimmi1@gmail.com>
2023-06-24 05:51:10 +0100

    Updated tool to work in python 3.11. Added note of required packages in tools

--- tools/setup.bat
+++ tools/setup.bat
@@ -1,4 +1,4 @@
-:: Requires twine to be installed
+:: Requires twine~=4.0.2 to be installed
 :: This script will build your package and attempt to upload it to pypi using the metadata in setup.py
 
 cd ..
